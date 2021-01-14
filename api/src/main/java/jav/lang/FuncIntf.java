245bd228b69ea0e0f17158c05e02e186652f8bf3
chenjianfeng <chenjianfeng@163.cn>
2021-01-14 11:48:47 +0800

    key=自定义函数式接口

---
+++ api/src/main/java/jav/lang/FuncIntf.java
@@ -0,0 +1,62 @@
+package jav.lang;
+
+import lombok.Data;
+
+/**
+ * @Description: 自定义函数式接口
+ * @Author
+ * @Date 2021/1/14
+ * @Version 1.0
+ */
+public class FuncIntf {
+
+    /*public static void main(String[] args) {
+        callNothing(() -> {
+            System.out.println("NOTHING 做一些快乐的事");
+        });
+    }
+
+    public static void callNothing(Comsumer comsumer) {
+        comsumer.nothing();
+    }
+
+    // 定义"函数式接口"时,为防止发生定义错误.可以使用@FunctionalInterface注解,强制按照"函数式接口"的语法检查,如果语法错误.编译器报错.
+    // 可以去掉@FunctionalInterface注解
+    @FunctionalInterface
+    public interface Comsumer {
+        void nothing();
+    }*/
+
+    public static void main(String[] args) {
+        callApply(666, (brand, brandExpand) -> {
+            String ret = brand.getBrandId() + "--" + brand.getName() + "--" + brandExpand.getWebsite();
+            System.out.println(ret);
+        }); // 实现
+    }
+
+    public static void callApply(int id, Comsumer<Brand, BrandExpand> comsumer) {
+        Brand brand = new Brand();
+        brand.setBrandId(9527L);
+        brand.setName("白度");
+
+        BrandExpand brandExpand = new BrandExpand();
+        brandExpand.setWebsite("https://www.baidu.com/");
+
+        comsumer.apply(brand, brandExpand); // 调用
+    }
+
+    public interface Comsumer<A, B> {
+        void apply(A a, B b);
+    }
+}
+
+@Data
+class Brand {
+    private Long brandId;
+    private String name;
+}
+
+@Data
+class BrandExpand {
+    private String website;
+}
