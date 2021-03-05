c1588a33304be94991ec94b3ccd6400f71580df5
chenjianfeng <chenjianfeng@163.cn>
2021-03-05 15:17:48 +0800

    get

--- api/src/main/java/or/apache/http/client/HttpClientUtil.java
+++ api/src/main/java/or/apache/http/client/HttpClientUtil.java
@@ -1,7 +1,5 @@
 package or.apache.http.client;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
@@ -16,13 +14,10 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
- * @Description:
+ * @Description: org.apache.httpcomponents » httpclient  CloseableHttpClient
  * @Author
  * @Date 2020/11/27
  * @Version 1.0
@@ -43,29 +38,34 @@ public class HttpClientUtil {
     private static final int CONNECT_TIME_OUT = 60000;
 
     /**
-     * key=CloseableHttpClient
+     * ##: get
      *
-     * @param uri
-     * @param defaultCharset
+     * @param url
+     * @param charset "UTF-8"
+     * @Description:
+     * @Author
+     * @Date
+     * @From
+     * @Function
+     * @Version 1.0
      */
-    public static void get(String uri, String defaultCharset) {
+    public static String get(String url, String charset) {
         // 创建HttpClient对象
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
         // 创建HttpGet对象，设置url访问地址
-        HttpGet httpGet = new HttpGet(uri);
+        HttpGet httpGet = new HttpGet(url);
 
         CloseableHttpResponse response = null;
+        String result = null;
         try {
             // 使用HttpClient发起请求，获取response
             response = httpClient.execute(httpGet);
 
-            //解析响应
+            // 解析响应
             if (response.getStatusLine().getStatusCode() == 200) {
-                String content = EntityUtils.toString(response.getEntity(), defaultCharset);
-                System.out.println(content);
+                result = EntityUtils.toString(response.getEntity(), charset);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -80,21 +80,30 @@ public class HttpClientUtil {
                 e.printStackTrace();
             }
         }
+        return result;
     }
 
     /**
-     * key=CloseableHttpClient
+     * ##: post
      *
      * @param url
-     * @param params
-     * @param defaultCharset
-     * @return
+     * @param charset "UTF-8"
+     * @Description:
+     * @Author
+     * @Date
+     * @From
+     * @Function
+     * @Version 1.0
      */
-    public static String post(String url, Map<String, Object> params, String defaultCharset) {
+    public static String post(String url, Map<String, Object> params, String charset) {
+        // Content-type
+
         // 创建HttpClient对象
         CloseableHttpClient httpClient;
+        HttpPost method;
         CloseableHttpResponse response;
-        HttpEntity entity;
+        HttpEntity entity = null;
+        String result = null;
 
         try {
             httpClient = HttpClients.createDefault();
@@ -102,22 +111,19 @@ public class HttpClientUtil {
                 建立Request的对象
                 Request一般配置addHeader、setEntity、setConfig
              */
-            HttpPost method = new HttpPost(url);
+            method = new HttpPost(url);
 
-            method.addHeader("Content-type","application/json; charset=utf-8");
-            method.setHeader("Accept", "application/json");
-
-            // setConfig,添加配置,如设置请求超时时间,连接超时时间
+            // 设置配置
             RequestConfig reqConfig = RequestConfig.custom()
-                    .setSocketTimeout(SOCKET_TIME_OUT)
-                    .setConnectTimeout(CONNECT_TIME_OUT).build();
+                    .setSocketTimeout(SOCKET_TIME_OUT) // 设置请求超时时间
+                    .setConnectTimeout(CONNECT_TIME_OUT) // 设置连接超时时间
+                    .build();
             method.setConfig(reqConfig);
 
-            // setEntity 设置请求参数
-            entity = new UrlEncodedFormEntity(createParam(params), Consts.UTF_8);
+            // 设置请求参数
+            entity = new UrlEncodedFormEntity(HttpClientUtilHelp.createParam(params), Consts.UTF_8);
             method.setEntity(entity);
 
-            // 执行Request请求
             response = httpClient.execute(method);
             entity = response.getEntity();
 
@@ -125,48 +131,17 @@ public class HttpClientUtil {
                 用EntityUtils.toString()这个静态方法将HttpEntity转换成字符串,
                 为防止服务器返回的数据带有中文,可以在转换的时候将字符集指定成utf-8
              */
-            return EntityUtils.toString(entity, defaultCharset);
+            result = EntityUtils.toString(entity, charset);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            // 一定要记得把entity fully consume掉，否则连接池中的connection就会一直处于占用状态
-//            EntityUtils.consume(entity);
-        }
-        return null;
-    }
-
-    private static List<NameValuePair> createParam(Map<String, Object> params) {
-        // 建立一个NameValuePair数组，用于存储请求参数
-        List<NameValuePair> nameValuePairs = new ArrayList<>();
-        if (params != null) {
-            for (String key : params.keySet()) {
-                nameValuePairs.add(new BasicNameValuePair(key, params.get(key).toString()));
+            try {
+                // 一定要记得把entity fully consume掉，否则连接池中的connection就会一直处于占用状态
+                EntityUtils.consume(entity);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        return nameValuePairs;
-    }
-
-    public static void main(String[] args) {
-        List<LadderProductVo> list = new ArrayList<>();
-        Map<String, Object> params = new HashMap<>();
-        params.put("ladderId", 1L);
-        params.put("pv", "苹果A14");
-        // params.put("callback", "callback");
-//        String url = "http://local.pconline.com.cn:8082/api/tiantitu/mini";
-        String url = "http://mall.pconline.com.cn/api/tiantitu/mini";
-        String ret = post(url, params, "UTF-8");
-        System.out.println("ret=" + ret);
-        JSONObject jsonObject = JSONObject.parseObject(ret);
-        JSONArray data = jsonObject.getJSONArray("data");
-        if (data != null && data.size() > 0) {
-            for (int i = 0; i < data.size(); i++) {
-                LadderProductVo vo = new LadderProductVo();
-                vo.setId(data.getJSONObject(i).getLong("productId"));
-                vo.setName(data.getJSONObject(i).getString("productName"));
-                list.add(vo);
-            }
-        }
-
-        System.out.println(list);
+        return result;
     }
 }
---
+++ api/src/main/java/or/apache/http/client/HttpClientUtilHelp.java
@@ -0,0 +1,21 @@
+package or.apache.http.client;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HttpClientUtilHelp {
+    public static List<NameValuePair> createParam(Map<String, Object> params) {
+        // 建立一个NameValuePair数组，用于存储请求参数
+        List<NameValuePair> nameValuePairs = new ArrayList<>();
+        if (params != null) {
+            for (String key : params.keySet()) {
+                nameValuePairs.add(new BasicNameValuePair(key, params.get(key).toString()));
+            }
+        }
+        return nameValuePairs;
+    }
+}
