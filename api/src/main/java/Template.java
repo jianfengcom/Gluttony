c1211507eecde8e95aec5849f521938a7bff65bc
chenjianfeng <chenjianfeng@163.cn>
2021-03-05 13:47:40 +0800

    IP

---
+++ api/src/main/java/Template.java
@@ -0,0 +1,32 @@
+public class Template {
+
+    /**
+     * ##: 爬虫代理IP
+     *
+     * @param url 抓取url
+     * @Description: 从360的软件终端页-pc获取apk下载链接 修改为 wap
+     * @Author chenjianfeng1
+     * @Date 2020/11/28
+     * @From ProxyIpUtils.java
+     * @Function 爬虫代理IP
+     * @Version 1.0
+     */
+    public static void main(String url) {
+
+    }
+
+    /**
+     * ##: 监听事件实现
+     *
+     * @param url 抓取url
+     * @Description: 监听事件, 允许异步并开启异步=SendEmailListener.onApplicationEvent()方法不用处理完一个事件对象才能处理一个事件对象
+     * @Author
+     * @Date
+     * @From
+     * @Function
+     * @Version 1.0
+     */
+    public static void none(String url) {
+
+    }
+}
--- api/src/main/java/cn/pc/IP.java
+++ api/src/main/java/cn/pc/IP.java
@@ -32,15 +32,24 @@ public class IP {
         if (null == ip || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         } else if (ip.length() > 15) {
-            String[] ips = ip.split(",");
-            for (int index = 0; index < ip.length(); index++) {
-                String strIp = (String) ips[index];
-                if (!("unknown".equalsIgnoreCase(strIp))) {
-                    ip = strIp;
-                    break;
-                }
-            }
+            String targetIp = getTargetIp(ip);
+            if (targetIp != null)
+                return targetIp;
         }
         return ip;
+    }
+
+    public static String getTargetIp(String source) {
+        if (source == null) {
+            source = "unknown, 120.240.100.33, 192.168.238.25, 192.168.238.153";
+        }
+        String[] ipList = source.split(",");
+        for(int index = 0; index < ipList.length; index ++){
+            String ip = ipList[index].trim();
+            if(!("unknown".equalsIgnoreCase(ip))){
+                return ip;
+            }
+        }
+        return null;
     }
 }
