731ddc5fb0c3ade77fa4f13aec23c4dfbe7f96ee
chenjianfeng <chenjianfeng@163.cn>
2021-03-07 21:55:49 +0800

    update

--- api/src/main/java/jav/util/DateUtil.java
+++ api/src/main/java/jav/util/DateUtil.java
@@ -3,22 +3,20 @@ package jav.util;
 import java.util.Calendar;
 import java.util.Date;
 
-/**
- * @Description:
- * @Author
- * @Date 2020/11/25
- * @Version 1.0
- */
 public class DateUtil {
-    public static final int ONE_DAY = 1;
+
+    public static final int NEST = 1;
 
     /**
-     * key=获取指定n天后的时间
-     * ps: n可以为负数
+     * ##: 获取指定n天后的时间
      *
-     * @param date
-     * @param number
-     * @return
+     * @param number 可以为负数
+     * @Description
+     * @Author chenjianfeng1
+     * @Date
+     * @From
+     * @Function
+     * @Version 1.0
      */
     public static Date next(Date date, int number) {
         Calendar c = Calendar.getInstance();
@@ -26,4 +24,5 @@ public class DateUtil {
         c.add(Calendar.DATE, number);
         return c.getTime();
     }
+
 }
