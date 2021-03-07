30743d41b70594505e772d16dd4a96acbf0dc144
chenjianfeng <chenjianfeng@163.cn>
2021-03-07 19:47:39 +0800

    update

--- api/src/main/java/cn/pc/Pcdlc.java
+++ api/src/main/java/cn/pc/Pcdlc.java
@@ -1,5 +1,10 @@
 package cn.pc;
 
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class Pcdlc {
 
     /**
@@ -23,4 +28,44 @@ public class Pcdlc {
         String number = url.substring(startStr).replace("/soft_id/", "");
         return wapHost + "/detail/&id=" + number;
     }
+
+    /**
+     * ##: url空格
+     *
+     * @Description
+     * @Author chenjianfeng1
+     * @Date
+     * @From
+     * @Function
+     * @Version 1.0
+     */
+    public static void spaceEncode(String original) {
+        if (original == null)
+            original = "https://ftp.pconline.com.cn/pub/download/201010/maldner_20200831/terminator/Microsoft Edge浏览器_84.0.522.61@1918_372423.exe?md5=RYhN34QCiOqfcoyqxG9z5A&expires=1599213920";
+
+        URL url = null;
+        try {
+            url = new URL(original);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        //  path=/pub/download/...
+        String path = url.getPath();
+
+        String headPath = path.substring(0, path.lastIndexOf("/") + 1);
+        String lastPath = null;
+        try {
+            lastPath = URLEncoder.encode(path.substring(path.lastIndexOf("/") + 1), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String targetPath = headPath + lastPath.replace("+", "%20"); // 解决方案: %20替换+
+
+        System.out.println(path);
+        System.out.println(headPath + lastPath);
+        System.out.println(targetPath);
+
+    }
+
 }
--- api/src/main/java/jav/lang/StringUtil.java
+++ api/src/main/java/jav/lang/StringUtil.java
@@ -1,53 +1,6 @@
 package jav.lang;
 
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-
-/**
- * @Description:
- * @Author
- * @Date 2020/11/10
- * @Version 1.0
- */
 public class StringUtil {
-
-    /**
-     * key=空格编码问题
-     *
-     * ps: pconline-dl
-     *
-     * @param spec
-     * @throws Exception
-     * @Author cjf-pc
-     */
-    public static void spaceEncode(String spec) {
-        if (spec == null)
-            spec = "https://ftp.pconline.com.cn/pub/download/201010/maldner_20200831/terminator/Microsoft Edge浏览器_84.0.522.61@1918_372423.exe?md5=RYhN34QCiOqfcoyqxG9z5A&expires=1599213920";
-
-        URL url = null;
-        try {
-            url = new URL(spec);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        String path = url.getPath(); //  path=/pub/download/...
-
-        String headPath = path.substring(0, path.lastIndexOf("/") + 1);
-        String lastPath = null;
-        try {
-            lastPath = URLEncoder.encode(path.substring(path.lastIndexOf("/") + 1), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        String targetPath = headPath + lastPath.replace("+", "%20"); // 解决方案: %20替换+
-
-        System.out.println(path);
-        System.out.println(targetPath);
-        System.out.println(headPath + lastPath);
-    }
 
     /**
      * key=分隔字符串
