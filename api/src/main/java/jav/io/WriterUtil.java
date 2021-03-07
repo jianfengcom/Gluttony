c4800a2548881576d68c0f4f1e66c3e2704d4d7e
chenjianfeng <chenjianfeng@163.cn>
2021-03-07 18:52:46 +0800

    update

--- api/src/main/java/jav/io/WriterUtil.java
+++ api/src/main/java/jav/io/WriterUtil.java
@@ -5,20 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-/**
- * @Description:
- * @Author
- * @Date 2020/11/17
- * @Version 1.0
- */
 public class WriterUtil {
 
-    /**
-     * key=Writer
-     *
-     * @param file
-     * @param content
-     * @throws IOException
+    /*
+        ##: 写入File
      */
     public static void write(File file, String content) throws IOException {
         FileOutputStream fos = new FileOutputStream(file);
