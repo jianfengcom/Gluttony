d3c77885cac1de0c326e0b77eb30146de3928235
chenjianfeng <chenjianfeng@163.cn>
2021-03-08 18:05:10 +0800

    update

--- api/src/main/java/jav/lang/ThreadDemo.java
+++ api/src/main/java/jav/lang/ThreadDemo.java
@@ -3,11 +3,8 @@ package jav.lang;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * @Description: key=线程池
- * @Author
- * @Date 2020/11/10
- * @Version 1.0
+/*
+    ##: wait 线程池
  */
 public class ThreadDemo {
     public static void main(String[] args) {
