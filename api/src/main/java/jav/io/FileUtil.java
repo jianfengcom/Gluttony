23c5c75a6a76145b8845a9d5c6ca776cce625d5e
bugger <bugger@email.com>
2021-06-01 15:32:41 +0800

    temp

--- api/src/main/java/jav/io/FileUtil.java
+++ api/src/main/java/jav/io/FileUtil.java
@@ -1,7 +1,14 @@
 package jav.io;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileUtil {
 
@@ -14,6 +21,15 @@ public class FileUtil {
 
         FileReader fileReader;
         try {
+            File target = new File("/temptemptemp/fk-question0601.sql");
+            if(!target.exists()) {
+                try {
+                    target.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
             result = new StringBuffer();
             // 创建一个读取文件的流对象 FileReader
             // 用BufferedReader封装一下，读取效率更高
@@ -23,11 +39,40 @@ public class FileUtil {
                 bufferedReader.readLine(): 读取文件的一行，如果有多行，会逐行读取。
              */
             String line;
+            List<String> tempList = new ArrayList<>();
             while ((line = bufferedReader.readLine()) != null) {
-                result.append(line);
+                JSONObject obj = JSON.parseObject(line);
+                if (obj.containsKey("wySuggestion") &&  (!"PASS".equals(obj.getString("wySuggestion")))) {
+                    long id = obj.getLongValue("id");
+                    String tableName = obj.getString("tableName");
+
+                    System.out.println(obj.getString("uri") + " - " + id);
+
+                    // 获取ID
+                    tempList.add("UPDATE " + tableName + " SET status = -1 WHERE status > 0 AND id = " + id + ";");
+                }
+
+                /*
+                一代目
+
+                Pattern pattern = Pattern.compile("//product.pconline.com.cn/pdlib/(\\d+)_comment_id(\\d+).html");
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()) {
+                    String group = matcher.group();
+                    if (true || group.contains("DELETE") || group.contains("REVIEW")) {
+                        int start = group.indexOf("_comment_id");
+                        int end = group.indexOf(".html");
+
+                        String s = group.substring(start + 11, end);
+                        System.out.println("UPDATE cmt_comment SET reviewStatus = -1  WHERE reviewStatus = 1 AND cmtCommentId = " + s + ";");
+                    }
+                }*/
 
                 // 控制台
                 // System.out.println(line);
+            }
+            if (!tempList.isEmpty()) {
+                WriterUtil.writeFile(target, tempList);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,6 +87,12 @@ public class FileUtil {
             }
         }
         return result.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(2333);
+        readFileContent(new File("D:/temptemptemp/house/censor_house_fk_question_2021.txt_wy_out"));
+        System.out.println(666);
     }
 
     /*
--- api/src/main/java/jav/io/WriterUtil.java
+++ api/src/main/java/jav/io/WriterUtil.java
@@ -1,9 +1,9 @@
 package jav.io;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.*;
+import java.util.List;
 
 public class WriterUtil {
 
@@ -11,10 +11,49 @@ public class WriterUtil {
         ##: 写入File
      */
     public static void write(File file, String content) throws IOException {
-        FileOutputStream fos = new FileOutputStream(file);
+        if(!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FileOutputStream fos = new FileOutputStream(file, true);
         OutputStreamWriter osw = new OutputStreamWriter(fos);
         osw.write(content);
         osw.close();
+    }
+
+    public static void writeFile(File file, List<String> list) throws IOException {
+        FileWriter fw = null;
+        PrintWriter pw = null;
+
+        try {
+            fw = new FileWriter(file, true); // 追加
+            pw = new PrintWriter(fw);
+
+            for (String obj : list) {
+                try {
+                    pw.println(obj);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } finally {
+            if (pw != null) {
+                pw.flush();
+            }
+            if (fw != null) {
+                fw.flush();
+            }
+            if (pw != null) {
+                pw.close();
+            }
+            if (fw != null) {
+                fw.close();
+            }
+        }
     }
 
 }
